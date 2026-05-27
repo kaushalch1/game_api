3b8c627eaae41813747de37e2215edee819fdc6a
kaushal <kaushal_chakka@srmap.edu.in>
2026-05-27 14:15:48 +0530

    rooms descirption

--- main.py
+++ main.py
@@ -24,27 +24,47 @@ class Action(BaseModel):
     choice:str
 ROOMS = {
     "lobby": {
-        "description": "You are at the edge of a dark forest. You see a path leading 'inside' and a shiny 'coin' on the ground.",
+        "description": "The central area conecting every room together.Players can rest here and choose where they can explore next.",
         "choices":[]
     },
     "brewery": {
-        "description": "It is pitch black. A goblin appears! You can 'fight' or 'run'.",
-        "choices": ["harvest_crops"]
+        "description": "The room smells of herbs and and old barrels.You can brew potions here and harvest herbs required for it",
+        "choices": ["brew","harvest"]
     },
     "deep_forest": {
-        "description": "You defeated the goblin and won the game! Restart the server to play again.",
-        "choices": ["Meditate"]
+        "description": "The room is full of trees where you can meditate and grow your health",
+        "choices": ["meditate"]
     },
     "cave":{
-        "description":"",
-        "choices":["Mine"]
+        "description":"You entered a cold dark cave .Water drips from the ceiling and stranger noises  echo the tunnels.You can mine here and use them for upgrading tools",
+        "choices":["mine"]
     },
     "quests":{
         "description":"",
         "choices":["Craft a gold pickaxe"]
     },
+    "library":{
+        "description":"Dusty books fill the ancient shelves.Candelight flickers and strange symbols.You can read ,solve riddle",
+        "choices":["read","riddle"]
+    },
+    "merchant":{
+        "description":"A merchant stands with a wagon of supplies and rare items.You can buy and sell items here",
+        "choices": ["buy","sell"]
+    },
+    "quests":{
+        "description":"A large wodden board is ccovered with quests and bounties.You can accept them",
+        "choices":["accept"]
+    },
+    "blacksmith":{
+        "description":"You can forge your tools like sword and pickaxe and also repair them if their health is less",
+        "choices":["upgrade","repair"]
+    },
+    "boss_fight":{
+        "description":"You step into a massive area.The ground shakes as a powerful enemy is hiding in the dark",
+        "choices":["enter"]
+    },
     "game_over": {
-        "description": "You died. Restart the server to try again.",
+        "description": "",
         "choices": []
     }
 }
@@ -62,7 +82,7 @@ def map():
         "|                   |                   |                   |\n"
         "+--------[ ]--------+--------[ ]--------+--------[ ]--------+\n"
         "|                   |                   |                   |\n"
-        "|       Quests      |    Boss Fight     |      Upgrade      |\n"
+        "|       Quests      |    Boss Fight     |     Blacksmith    |\n"
         "|                   |                   |                   |\n"
         "+-------------------+--------[ ]--------+-------------------+\n"
         "                          |       |\n"
@@ -70,7 +90,7 @@ def map():
         "                          |       |\n"
         "                          [  EXIT ] \n"
     )
-@app.get("/status")
+@app.get("/explore_room")
 def status():
     return{
         "location":player.location,
@@ -78,7 +98,7 @@ def status():
         "choices":ROOMS.get(player.location,{}).get("choices",[])
     }
 
-LOCATIONS=["lobby","cave","brewery","library","deep_forest","merchant","quests","boss_fight","upgrade"]
+LOCATIONS=["lobby","cave","brewery","library","deep_forest","merchant","quests","boss_fight","blacksmith"]
 @app.post("/teleport")
 def teleport(action: ActionRequest):
     if not action.teleport:
@@ -92,13 +112,14 @@ def teleport(action: ActionRequest):
     return PlainTextResponse(f"Player has teleported from {x} to {player.location}")
 @app.post("/choice")
 def choice(action:Action):
+    action.choice=action.choice.lower()
     if not action.choice:
         options=ROOMS.get(player.location, {}).get("choices", [])
         return PlainTextResponse(f"Please select a choice.Available choices:{options}")
     if action.choice not in ROOMS.get(player.location, {}).get("choices", []):
         options=ROOMS.get(player.location,{}).get("choices",[])
         return PlainTextResponse(f"Please select a choice.Available choices:{options}")
-    # if(player.location=="brewery"):
+    #if(player.location=="brewery"):
     #     if(action.choice=="")
 @app.get("/playerinfo")
 def info():
